8ff0619d07bb665960db3a2abbff16c26a1dbade
Joseff361 <joseff361@outlook.com>
2020-08-18 16:17:10 -0500

    Reactive Forms Part 3

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -13,6 +13,34 @@ export class ContactComponent implements OnInit {
   feedback: Feedback;
   contactType = ContactType;
 
+  formErrors = {
+    'firstname': '',
+    'lastname': '',
+    'telnum': '',
+    'email': ''
+  };
+
+  validationMessages = {
+    'firstname': {
+      'required' : 'First Name is required.',
+      'minlength': 'First Name must be at least 2 characters long.',
+      'maxlength': 'FirstName cannot be more than 25 characters long.'
+    },
+    'lastname': {
+      'required' : 'Last Name is required.',
+      'minlength': 'Last Name must be at least 2 characters long.',
+      'maxlength': 'Last Name cannot be more than 25 characters long.'
+    },
+    'telnum': {
+      'required' : 'Tel. number is required.',
+      'pattern'  : 'Tel. number must contain only numbers.'
+    },
+    'email': {
+      'required' : 'Email is required.',
+      'email'    : 'Email not in valid format.'
+    },
+  };
+
   //Get access any of the child child DOM elements whitin my template
   @ViewChild('fform') feedbackFormDirective;
 
@@ -24,16 +52,46 @@ export class ContactComponent implements OnInit {
   ngOnInit(): void {
   }
 
- createForm() {
+ createForm(): void {
     this.feedbackForm = this.fb.group({
-      firstname: ['', Validators.required ],
-      lastname: ['', Validators.required ],
-      telnum: ['', Validators.required ],
-      email: ['', Validators.required ],
+      firstname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)] ],
+      lastname: ['', [Validators.required, Validators.minLength(2), Validators.maxLength(25)] ],
+      telnum: ['', [Validators.required, Validators.pattern] ],
+      email: ['', [Validators.required, Validators.email] ],
       agree: false,
       contacttype: 'None',
       message: ''
     });
+
+    this.feedbackForm.valueChanges //valueChanges is an observable
+      .subscribe(data => this.onValueChanged(data));
+
+    this.onValueChanged(); // (re)set form validation messages 
+
+  }
+
+  onValueChanged(data?: any) { //parameter is optional
+    if (!this.feedbackForm) { return; } //has been created?
+    
+    const form = this.feedbackForm;
+    
+    for (const field in this.formErrors) {
+      if (this.formErrors.hasOwnProperty(field)) {//make sure that the object contain the property
+        
+        this.formErrors[field] = ''; // clear previous error message (if any)
+
+        const control = form.get(field); //const form = this.feedbackForm;
+
+        if (control && control.dirty && !control.valid) {
+          const messages = this.validationMessages[field];
+          for (const key in control.errors) { // const control = form.get(field);
+            if (control.errors.hasOwnProperty(key)) {
+              this.formErrors[field] += messages[key] + ' ';
+            }
+          }
+        }
+      }
+    }
   }
 
   onSubmit() {
